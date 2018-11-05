6fa328f07e94cb274f9b0f734b3ce058b42fb92a
Iván Mota <imota@antevenio.com>
2018-11-05 18:54:41 +0100

    Work in progress, trying to test it all.

--- test/OAuth2/Client/Provider/MDirectorTest.php
+++ test/OAuth2/Client/Provider/MDirectorTest.php
@@ -2,8 +2,14 @@
 
 namespace MDOAuth\Test\OAuth2\Client\Provider;
 
+use GuzzleHttp\Client;
+use League\OAuth2\Client\Grant\AbstractGrant;
+use League\OAuth2\Client\Grant\GrantFactory;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\RequestFactory;
 use MDOAuth\OAuth2\Client\Provider\MDirector;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 class MDirectorTest extends TestCase
 {
@@ -11,16 +17,106 @@ class MDirectorTest extends TestCase
      * @var MDirector
      */
     protected $sut;
+    /**
+     * @var GrantFactory | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $grantFactory;
+    protected $requestFactory;
+    /**
+     * @var Client | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $httpClient;
+
+    protected $clientId;
 
     public function setUp()
     {
-        $this->sut = new MDirector();
+        // TODO: Should this be provided by the oauth provider?
+        $this->clientId = 'mock_client_id';
+        $this->grantFactory = $this->createMock(GrantFactory::class);
+        $this->httpClient = $this->createMock(Client::class);
+        // TODO: Do i have to mock this last one?
+        // $this->requestFactory = $this->createMock(RequestFactory::class);
+
+        $this->sut = new MDirector(
+            [
+                // TODO: Should this be provided by the oauth provider?
+                'clientId' => $this->clientId,
+            ],
+            [
+                'grantFactory' => $this->grantFactory,
+                //'requestFactory' => $this->requestFactory,
+                'httpClient' => $this->httpClient
+            ]
+        );
     }
 
-    public function testProviderShouldHaveProperClientIdSet()
+    public function testShouldBeCreated()
     {
-        $headers =  $this->sut->getHeaders();
-        var_dump($headers);
-        $this->assertEquals('webapp', $this->sut->getHeaders());
+        $this->assertInstanceOf(MDirector::class, $this->sut);
+    }
+
+    public function testGetBaseAuthorizationUrl()
+    {
+        $this->assertEquals(
+            'https://app.mdirector.com/oauth2-authorize',
+            $this->sut->getBaseAuthorizationUrl()
+        );
+    }
+
+    public function testGetBaseAccessTokenUrl()
+    {
+        $this->assertEquals(
+            'https://app.mdirector.com/oauth2',
+            $this->sut->getBaseAccessTokenUrl([])
+        );
+    }
+
+    public function testGetResourceOwnerDetailsUrl()
+    {
+        /** @var \League\OAuth2\Client\Token\AccessToken $accessToken */
+        $accessToken = $this->createMock(AccessToken::class);
+        $this->assertEquals(
+            'https://app.mdirector.com/oauth2-api',
+            $this->sut->getResourceOwnerDetailsUrl($accessToken)
+        );
+    }
+
+    public function testGetAccessToken()
+    {
+        $grantName = 'someGrant';
+        $options = ['option1' => '1', 'option2' => '2'];
+
+        $grant = $this->createMock(AbstractGrant::class);
+        $preparedRequestParameters = [];
+        $grant->expects($this->once())
+            ->method('prepareRequestParameters')
+            ->with(
+                $this->callback(function ($params) {
+                    $this->assertArrayHasKey('client_id', $params);
+                    $this->assertEquals($this->clientId, $params['client_id']);
+                    return true;
+                }),
+                $this->equalTo($options)
+            )
+            ->will($this->returnValue($preparedRequestParameters));
+
+        $this->grantFactory->expects($this->once())
+            ->method('getGrant')
+            ->with($this->equalTo($grantName))
+            ->will($this->returnValue($grant));
+
+        $response = $this->createMock(ResponseInterface::class);
+
+        $this->httpClient->expects($this->once())
+            ->method('send')
+            ->with($this->callback(function ($haha) {
+                //TODO: Should we mock the request preparation?, most probably YES
+                var_dump($haha);
+                return true;
+            }))
+            ->will($this->returnValue($response));
+
+        $this->sut->getAccessToken($grantName, $options);
     }
 }
--- test/OAuth2/MDirectorTest.php
+++ test/OAuth2/MDirectorTest.php
@@ -1,13 +1,8 @@
 <?php
 namespace MDOAuth\Test\OAuth2;
 
-use GuzzleHttp\ClientInterface;
-use League\OAuth2\Client\Provider\GenericProvider;
-use League\OAuth2\Client\Token\AccessToken;
-use MDOAuth\OAuth2\MDirector;
-use PHPUnit\Framework\MockObject\MockObject;
+use MDOAuth\OAuth2\Client\MDirector;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\RequestInterface;
 
 class MDirectorTest extends TestCase
 {
@@ -16,69 +11,24 @@ class MDirectorTest extends TestCase
      */
     protected $sut;
 
-    protected $accessTokenUrl;
     protected $key;
     protected $secret;
 
     protected $uri;
     /**
-     * @var GenericProvider | MockObject
+     * @var \MDOAuth\OAuth2\Client\Provider\MDirector | \PHPUnit_Framework_MockObject_MockObject
      */
     protected $provider;
-    /**
-     * @var AccessToken | MockObject
-     */
-    protected $accessToken;
-    /**
-     * @var ClientInterface | MockObject
-     */
-    protected $httpClient;
-    /**
-     * @var RequestInterface | MockObject
-     */
-    protected $request;
 
     public function setUp()
     {
-        $this->accessTokenUrl = 'http://some.url/l';
         $this->key = 'someKey';
         $this->secret = 'someSecret';
-        $this->uri = 'https://some.url/k';
-        $this->provider = $this->createMock(GenericProvider::class);
-        $this->accessToken = $this->createMock(AccessToken::class);
-        $this->httpClient = $this->createMock(ClientInterface::class);
-        $this->request = $this->createMock(RequestInterface::class);
-        $this->sut = new MDirector($this->provider, $this->key, $this->secret);
+        $this->sut = new MDirector($this->key, $this->secret);
     }
 
     public function testShouldBeCreated()
     {
         $this->assertInstanceOf(MDirector::class, $this->sut);
-    }
-
-    public function testRequestShouldGetANewAccessToken()
-    {
-        $this->provider->expects($this->once())
-            ->method('getAccessToken')
-            ->with(
-                $this->equalTo('password'),
-                $this->equalTo(
-                    [
-                        'username' => $this->key,
-                        'password' => $this->secret
-                    ]
-                )
-            )
-            ->will($this->returnValue($this->accessToken));
-
-        $this->provider->expects($this->any())
-            ->method('getHttpClient')
-            ->will($this->returnValue($this->httpClient));
-
-        $this->provider->expects($this->any())
-            ->method('getAuthenticatedRequest')
-            ->will($this->returnValue($this->request));
-
-        $this->sut->setMethod('get')->setUri($this->uri)->request();
     }
 }
